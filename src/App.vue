495eaf930d5269714a46c0a97570c5d21a300227
EngiFrost <engifrost@gmail.com>
2022-09-19 20:23:15 +0300

    rendered posts

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
+  <div id="app">
+    <div class="post" v-for="post in posts" :key="post.id">{{post.title}}</div>
+  </div>
 </template>
 
 <script>
-
+// TODO: convert to Composition API
 export default {
   name: 'App',
+  data() {
+    return {
+      posts: []
+    }
+  },
+  async mounted() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users/1/posts?_limit=3')
+    const posts = await response.json()
+    this.posts = posts
+  }
 }
 </script>
 
@@ -16,5 +29,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.post {
+  padding: 20px;
+  border: 1px solid black;
+  border-radius: 5px;
+  margin: 10px auto;
 }
 </style>
